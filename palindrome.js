c9b6d3faa33efff5a84a389c1008460bcb8fa358
James Sutcliffe <jamessutcliffe2012@hotmail.co.uk>
2022-11-29 21:27:43 +0000

    reverse function added / preliminary if else statements made / final dom html write

--- palindrome.js
+++ palindrome.js
@@ -1,8 +1,13 @@
 
-
 function palindrome(){
     let entry = document.getElementById('text1').value;
-    console.log(entry);
+    let cleanEntry = entry.replace(/[^a-zA-Z0-9 ]/g, '');
+    let lowerCaseEntry = cleanEntry.toLowerCase();
+
+    //create reverse of lower case entry//
+    // the if else statements writing html and apppending whether entry is palindrone // 
+
+    console.log(lowerCaseEntry);
 }
 
 const chips = document.getElementById('btn1');
